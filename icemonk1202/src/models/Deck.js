5a0a7f020631a38665518bfe1349663c5d4f65e8
icymonk <icemonk1202@gmail.com>
2020-05-30 18:30:31 +0900

    Add models
    add Card, Deck, Token, Player

---
+++ icemonk1202/src/models/Deck.js
@@ -0,0 +1,14 @@
+export default class Deck {
+  level
+  cards
+
+  constructor(level, cards) {
+    this.level = level
+    this.cards = cards
+  }
+  suffle() {
+    /**
+     * @TODO cards suffle구현
+     */
+  }
+}
