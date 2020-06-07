8c71579ab70c56a3d39e2df76ca9a3f680e31e5b
Jason Kim <actcrazy@gmail.com>
2020-06-07 16:05:29 +0900

    feat: 카드 구입 검증 로직 구현

--- actcrazy/src/splendor/lib/__spec__/game.spec.ts
+++ actcrazy/src/splendor/lib/__spec__/game.spec.ts
@@ -1,4 +1,4 @@
-import { validateGetTokens, createTokens } from '../game'
+import { validateGetTokens, Board, createTokens, createDevelopment, createGems, validateBuyCard, addTokens, addDevelopment } from '../game'
 
 describe('토큰 가져오기', () => {
   const emptyTokens = createTokens(0, 0, 0, 0, 0);
@@ -35,4 +35,58 @@ describe('토큰 가져오기', () => {
 })
 
 describe('카드 구입하기', () => {
+  const card1 = createDevelopment(1, 0, createGems(2, 2, 0, 0), createGems(0, 0, 0, 0));
+  const card2 = createDevelopment(1, 0, createGems(2, 1, 0, 0), createGems(1, 0, 0, 0));
+  const card3 = createDevelopment(2, 0, createGems(2, 1, 1, 0), createGems(0, 0, 0, 0));
+  const card4 = createDevelopment(2, 0, createGems(3, 2, 0, 0), createGems(0, 0, 0, 0));
+
+  const board: Board = {
+    tokens: createTokens(0, 0, 0, 0, 0), 
+    noble: [],
+    deck: { 1: [], 2: [], 3: [] },
+    matt: { 1: [card1], 2: [card3, card4], 3: [] },
+    hands: {
+      p1: {
+        tokens: createTokens(0, 0, 0, 0, 0), 
+        development: [],
+        reserved: [card2],
+        noble: [],
+      }
+    }
+  };
+
+  it('덱에 없는 카드는 구입할 수 없다.', () => {
+    const ret = validateBuyCard(board, 'p1', [1, 1]);
+    expect(ret).toBe(false)
+  })
+
+  it('예약목록에 없는 카드는 구입할 수 없다.', () => {
+    const ret = validateBuyCard(board, 'p1', 1);
+    expect(ret).toBe(false)
+  })
+
+  it('구매할 카드 비용이상의 보석 토큰을 가지고 있으면 카드 구입이 가능하다.', () => {
+    const b = addTokens('p1', createTokens(2, 2, 0, 0, 0), board);
+    const ret = validateBuyCard(b, 'p1', [1, 0]);
+    expect(ret).toBe(true)
+  })
+
+  it('구매할 카드 비용보다 가지고 있는 보석 토큰이 부족하면 카드를 구입할 수 없다.', () => {
+    const b = addTokens('p1', createTokens(2, 1, 0, 0, 0), board);
+    const ret = validateBuyCard(b, 'p1', [1, 0]);
+    expect(ret).toBe(false)
+  })
+
+  it('부족한 토큰을 황금 토큰으로 대체할 수 있다.', () => {
+    const b = addTokens('p1', createTokens(2, 1, 0, 0, 1), board);
+    const ret = validateBuyCard(b, 'p1', [1, 0]);
+    expect(ret).toBe(true)
+  })
+
+  it('손에 있는 카드의 할인을 적용 받는다.', () => {
+    const b1 = addDevelopment('p1', card2, board);
+    const b2 = addTokens('p1', createTokens(1, 2, 0, 0, 0), b1);
+    const ret = validateBuyCard(b2, 'p1', [1, 0]);
+    expect(ret).toBe(true)
+  })
 })
--- actcrazy/src/splendor/lib/game.ts
+++ actcrazy/src/splendor/lib/game.ts
@@ -22,7 +22,12 @@ export const createTokens = (emerald: number, diamond: number, sapphire: number,
 })
 
 type F = <T>(x: T, y: T) => T;
-export const evalTokens = (f: F) => (x: Tokens, y: Tokens): Tokens => R.mapObjIndexed((v, k) => f(v, y[k]), x)
+export const evalTokens = (f: F) => R.curry((x: Tokens, y: Tokens): Tokens => R.mapObjIndexed((v, k) => f(v, y[k]), x))
+export const evalGems = (f: F) => R.curry((x: Gems, y: Gems): Gems => {
+  const x1: Gems = R.pickAll(['emerald', 'diamond', 'sapphire', 'onyx'], x);
+  const y1: Gems = R.pickAll(['emerald', 'diamond', 'sapphire', 'onyx'], y);
+  return R.mapObjIndexed((v, k) => f(v, y1[k]), x1)
+})
 
 type Level = 1 | 2 | 3;
 interface Development {
@@ -46,6 +51,7 @@ interface Noble {
 interface Hand {
   tokens: Tokens;
   development: readonly Development[];
+  reserved: readonly Development[];
   noble: readonly Noble[];
 };
 
@@ -54,12 +60,18 @@ type Player = string | number;
 export interface Board {
   tokens: Tokens;
   noble: readonly Noble[];
-  deck: readonly [readonly Development[], readonly Development[], readonly Development[]];
-  matt: readonly [readonly Development[], readonly Development[], readonly Development[]];
+  deck: { [key in Level]: Development[] };
+  matt: { [key in Level]: Development[] };
   hands: {
     [key in Player]: Hand;
   }
 }
+
+export const addTokens = (p: Player, tokens: Tokens, b: Board): Board =>
+  R.assocPath(['hands', p, 'tokens'], evalTokens(R.add)(b.hands[p].tokens, tokens), b);
+
+export const addDevelopment = (p: Player, card: Development, b: Board): Board =>
+  R.over(R.lensPath(['hands', p, 'development']), R.append(card), b);
 
 export const validateGetTokens = (boardTokens: Tokens, tokens: Tokens): boolean => {
   const t = R.values(tokens);
@@ -95,3 +107,80 @@ export const validateGetTokens = (boardTokens: Tokens, tokens: Tokens): boolean 
 
   return false;
 }
+
+export const validateBuyCard = (b: Board, p: Player, pos: [Level, number] | number) => {
+  let card: Development | undefined = undefined;
+  if (typeof pos === 'number') {
+    card = b.hands[p].reserved[pos];
+  } else {
+    const [level, p] = pos;
+    card = b.matt[level][p];
+  }
+
+  if (R.isNil(card)) {
+    return false;
+  }
+
+  const tokens = b.hands[p].tokens;
+  if (R.isNil(tokens)) {
+    return false;
+  }
+
+  const discount: Gems = R.reduce(
+    evalGems(R.add),
+    createGems(0, 0, 0, 0),
+    R.pluck('discount', b.hands[p].development));
+
+  const cost = evalGems(R.subtract)(card.cost, discount);
+  const change = evalGems(R.subtract)(tokens, cost);
+  const sumLack = R.sum(R.filter(x => x <= 0, R.values(change)))
+  const isLack = sumLack + tokens.gold < 0;
+  if (isLack) {
+    return false;
+  }
+
+  return true;
+}
+
+/*
+const Splendor = {
+  name: "splendor",
+
+  setup: () => ({
+    cells: Array(9).fill(null)
+  }),
+
+  moves: {
+    clickCell(G, ctx, id) {
+      if (G.cells[id] === null) {
+        G.cells[id] = ctx.currentPlayer
+      }
+    }
+  },
+
+  turn: { moveLimit: 1 },
+
+  endIf: (G, ctx) => {
+    if (IsVictory(G.cells)) {
+      return { winner: ctx.currentPlayer }
+    }
+    if (G.cells.filter(c => c === null).length === 0) {
+      return { draw: true }
+    }
+  },
+
+  ai: {
+    enumerate: G => {
+      let moves = [];
+      for (let i = 0; i < 9; i++) {
+        if (G.cells[i] === null) {
+          moves.push({ move: "clickCell", args: [i] })
+        }
+      }
+      return moves
+    }
+  }
+}
+
+export default TicTacToe
+*/
