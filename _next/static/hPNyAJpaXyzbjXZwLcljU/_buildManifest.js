3c54fbbc6e92bc98d3f28f4d67eefce6a897ac74
김계승 <kimkyeseung@gmail.com>
2020-06-13 14:43:28 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/hPNyAJpaXyzbjXZwLcljU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/Xbluesyan":[a],"/actcrazy":[a],"/azanghs":[a],"/icemonk1202":[a],"/jiwon":[a],"/kimkyeseung":[a],"/ycfana":[a],"/zxcasd":[a]}}("static\u002Fchunks\u002F210028be80582f858e9ae926c3c551cf5237b72b.dc867cb915d6c77568e4.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
