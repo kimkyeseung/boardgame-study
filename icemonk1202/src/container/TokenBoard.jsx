d888ee687914ed0e70e1669b14552315e246ef0c
icymonk <icemonk1202@gmail.com>
2020-06-27 19:21:35 +0900

    Fix boardToken

--- icemonk1202/src/container/TokenBoard.jsx
+++ icemonk1202/src/container/TokenBoard.jsx
@@ -165,27 +165,29 @@ class TokenBoard extends Component {
     return (
       <>
         <TokenArea>
-          {Object.values(COLOR).map((color) => (
-            <div key={`token-${getId()}`} style={{ display: "flex" }}>
-              <Token
-                onClick={this.onClickToken}
-                onDoubleClick={this.onDoubleClickToken}
-                selected={this.state.selectedTokens[color]}
-                count={
-                  this.props.tokens.filter((token) => token.color === color)
-                    .length
-                }
-                color={color}
-              />
-              <TokenInput
-                value={this.state.selectedTokens[color]}
-                onChange={(event) => this.onChangeToken(event, color)}
-                type="number"
-                min="0"
-                max="2"
-              ></TokenInput>
-            </div>
-          ))}
+          {Object.values(COLOR)
+            .filter((color) => color !== "yellow")
+            .map((color) => (
+              <div key={`token-${getId()}`} style={{ display: "flex" }}>
+                <Token
+                  onClick={this.onClickToken}
+                  onDoubleClick={this.onDoubleClickToken}
+                  selected={this.state.selectedTokens[color]}
+                  count={
+                    this.props.tokens.filter((token) => token.color === color)
+                      .length
+                  }
+                  color={color}
+                />
+                <TokenInput
+                  value={this.state.selectedTokens[color]}
+                  onChange={(event) => this.onChangeToken(event, color)}
+                  type="number"
+                  min="0"
+                  max="2"
+                ></TokenInput>
+              </div>
+            ))}
           <Token
             count={
               this.props.tokens.filter((token) => token.color === "yellow")
