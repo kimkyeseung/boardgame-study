3361e190417a2eed65869d4de4f27fac308706f1
김계승 <kimkyeseung@gmail.com>
2020-07-04 19:20:29 +0900

    손에든 토큰 가져올 수 있는지 발리데이터 설정

--- kimkyeseung/src/lib/game.js
+++ kimkyeseung/src/lib/game.js
@@ -1,131 +1,125 @@
 import developmentCards from '../../assets/developmentCards.json'
+import { getTokenValidator } from './validator'
 
-function IsVictory(cells) {
-  const positions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-
-  const isRowComplete = row => {
-    return false
-  }
-
-  return positions.map(isRowComplete).some(i => i === true)
-}
-
-const Splendor = {
-  name: "splendor",
-
-  setup: ({ numPlayers, random, ...G }) => {
-    // console.log(G)
-    const developOneDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 1))
-    const developTwoDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 2))
-    const developThreeDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 3))
-    const board = {}
-    board.dev10 = developOneDeck.pop()
-    board.dev11 = developOneDeck.pop()
-    board.dev12 = developOneDeck.pop()
-    board.dev13 = developOneDeck.pop()
-
-    board.dev20 = developTwoDeck.pop()
-    board.dev21 = developTwoDeck.pop()
-    board.dev22 = developTwoDeck.pop()
-    board.dev23 = developTwoDeck.pop()
-
-    board.dev30 = developThreeDeck.pop()
-    board.dev31 = developThreeDeck.pop()
-    board.dev32 = developThreeDeck.pop()
-    board.dev33 = developThreeDeck.pop()
-
-    const tokenCount = numPlayers * 2 - 1 + (numPlayers === 2 ? 1 : 0)
-    const tokens = {}
-    tokens.red
-      = tokens.blue
-      = tokens.black
-      = tokens.white
-      = tokens.green
-      = tokenCount
-    tokens.yellow = 5
-
-    const fields = {}
-    const defaultValues = { white: 0, red: 0, blue: 0, green: 0, black: 0 }
-    Array(numPlayers).fill(1).forEach((a, i) => {
-      fields[`player${i}`] = {
-        developments: { ...defaultValues },
-        token: { ...defaultValues },
-        victoryPoints: 0
-      }
-    })
-    return {
-      fields,
-      board,
-      tokens,
-      developOneDeck,
-      developTwoDeck,
-      developThreeDeck,
-    }
-  },
-
-  moves: {
-    replaceDevelopmentSpace(G, ctx, { index, grade }) {
-      const deck = {
-        '1': G.developOneDeck,
-        '2': G.developTwoDeck,
-        '3': G.developThreeDeck
-      }
-      G.board[`dev${grade}${index}`] = deck[grade].pop()
-    },
-    buyDevelopment(G, ctx, development) {
-      console.log({ ctx })
-      console.log(development)
-      const { value, valueAmount, victoryPoint } = development
-
-      const { developments, victoryPoints } = G.fields[`player${ctx.currentPlayer}`]
-      developments[value]++
-      G.fields[`player${ctx.currentPlayer}`].victoryPoints = victoryPoints + victoryPoint
-      ctx.events.endTurn()
-    },
-    selectToken(G, ctx, token) {
-      console.log('select')
-    },
-    getTokens(G, ctx) {
-      console.log('work', { G, ctx })
-      // ctx.events.endTurn()
-    },
-
-  },
-
-  turn: {
-    // endIf: (G, ctx) => ({ next: '3' }),
-  },
-
-  endIf: (G, ctx) => {
-    return false
-    // if (IsVictory(G.cells)) {
-    //   return { winner: ctx.currentPlayer }
-    // }
-    // if (G.cells.filter(c => c === null).length === 0) {
-    //   return { draw: true }
-    // }
-  },
-
-  ai: {
-    enumerate: G => {
-      let moves = [];
-      for (let i = 0; i < 9; i++) {
-        if (G.cells[i] === null) {
-          moves.push({ move: "clickCell", args: [i] })
+const game = (playerNames) => {
+  const Splendor = {
+    name: "splendor",
+  
+    setup: ({ numPlayers, random, ...G }) => {
+      // console.log(G)
+      const developOneDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 1))
+      const developTwoDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 2))
+      const developThreeDeck = random.Shuffle(developmentCards.filter(({ grade }) => grade === 3))
+      const board = {}
+      board.dev10 = developOneDeck.pop()
+      board.dev11 = developOneDeck.pop()
+      board.dev12 = developOneDeck.pop()
+      board.dev13 = developOneDeck.pop()
+  
+      board.dev20 = developTwoDeck.pop()
+      board.dev21 = developTwoDeck.pop()
+      board.dev22 = developTwoDeck.pop()
+      board.dev23 = developTwoDeck.pop()
+  
+      board.dev30 = developThreeDeck.pop()
+      board.dev31 = developThreeDeck.pop()
+      board.dev32 = developThreeDeck.pop()
+      board.dev33 = developThreeDeck.pop()
+  
+      const tokenCount = numPlayers * 2 - 1 + (numPlayers === 2 ? 1 : 0)
+      const tokens = {}
+      tokens.red
+        = tokens.blue
+        = tokens.black
+        = tokens.white
+        = tokens.green
+        = tokenCount
+      tokens.yellow = 5
+  
+      const fields = {}
+      const defaultValues = { white: 0, red: 0, blue: 0, green: 0, black: 0 }
+      Array(numPlayers).fill(1).forEach((a, i) => {
+        fields[`player${i}`] = {
+          name: playerNames[i],
+          developments: { ...defaultValues },
+          token: { ...defaultValues },
+          reservedDevs: [],
+          hand: [],
+          victoryPoints: 0
         }
+      })
+      return {
+        fields,
+        board,
+        tokens,
+        developOneDeck,
+        developTwoDeck,
+        developThreeDeck,
       }
-      return moves
+    },
+  
+    moves: {
+      replaceDevelopmentSpace(G, ctx, { index, grade }) {
+        const deck = {
+          '1': G.developOneDeck,
+          '2': G.developTwoDeck,
+          '3': G.developThreeDeck
+        }
+        G.board[`dev${grade}${index}`] = deck[grade].pop()
+      },
+      buyDevelopment(G, ctx, development) {
+        console.log({ ctx })
+        console.log(development)
+        const { value, valueAmount, victoryPoint } = development
+  
+        const { developments, victoryPoints } = G.fields[`player${ctx.currentPlayer}`]
+        developments[value]++
+        G.fields[`player${ctx.currentPlayer}`].victoryPoints = victoryPoints + victoryPoint
+        ctx.events.endTurn()
+      },
+      selectToken(G, ctx, token) {
+        const { tokens, fields } = G
+        const { hand } = fields[`player${ctx.currentPlayer}`]
+        if (tokens[token]) {
+          G.tokens[token]--
+          hand.push(token)
+        }
+        const confirmable = getTokenValidator(hand)
+      },
+      getTokens(G, ctx) {
+        console.log('work', { G, ctx })
+        // ctx.events.endTurn()
+      },
+  
+    },
+  
+    turn: {
+      // endIf: (G, ctx) => ({ next: '3' }),
+    },
+  
+    endIf: (G, ctx) => {
+      return false
+      // if (IsVictory(G.cells)) {
+      //   return { winner: ctx.currentPlayer }
+      // }
+      // if (G.cells.filter(c => c === null).length === 0) {
+      //   return { draw: true }
+      // }
+    },
+  
+    ai: {
+      enumerate: G => {
+        let moves = [];
+        for (let i = 0; i < 9; i++) {
+          if (G.cells[i] === null) {
+            moves.push({ move: "clickCell", args: [i] })
+          }
+        }
+        return moves
+      }
     }
   }
+  return Splendor
 }
 
-export default Splendor
+export default game
