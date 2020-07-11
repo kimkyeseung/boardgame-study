2b9e3d3cf691fae8e550283a4630de9e7c70bb04
김계승 <kimkyeseung@gmail.com>
2020-07-11 15:49:15 +0900

    token 쌓여있는 모습 구현

--- kimkyeseung/src/components/Token.jsx
+++ kimkyeseung/src/components/Token.jsx
@@ -5,20 +5,34 @@ import styled from 'styled-components'
 const StyledToken = styled.div`
   display: flex;
   justify-content: space-between;
-  & > .token {
-    width: 100px; height: 100px;
-    border-radius: 100%;
-    border: 12px solid ${({ color }) => color};
-    box-sizing: border-box;
-  }
+  & > .tokenBundle {
+    position: relative;
+    width: 120px; height: 120px;
+  } 
   & > .count {
 
   }
 `
 
+const One = styled.div`
+  position: absolute;
+  width: 100px; height: 100px;
+  border-radius: 100%;
+  border: 12px solid ${({ color }) => color};
+  box-sizing: border-box;
+  background: lightgray;
+  -webkit-box-shadow: 2px 2px 4px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 2px 2px 4px 0px rgba(0,0,0,0.75);
+  box-shadow: 2px 2px 4px 0px rgba(0,0,0,0.75);
+  top: ${({ index }) => `${index * 4}px`};
+  left: ${({ index }) => `${index * 4}px`};
+`
+
 const Token = ({ color, count, ...props }) => (
-  <StyledToken color={color} {...props}>
-    <div className="token"></div>
+  <StyledToken {...props}>
+    <div className="tokenBundle">
+      {Array(count).fill().map((num, i) => <One key={i} index={i} color={color} className="token"></One>)}
+    </div>
     <div className="count">{count}</div>
   </StyledToken>
 )
